5c9eb4d1caf66623b691e83b9d50e5fa7df512c3
Jutsn <jusleyser@gmail.com>
2026-04-11 01:51:28 +0200

    bullet cooldown implemented

--- Player.py
+++ Player.py
@@ -1,27 +1,30 @@
 import pygame
 from Projectiles import ProjectileManager
 
+
 class PlayerController():
 
 	def __init__(self):
 		self.position = pygame.Vector2(400, 300)
+		self.last_shot_time = 0
+		self.cooldown = 300
 	
 	def update(self):
-		self.get_input()
+		self.input()
 		self.clamp_player_pos()
 
 	def draw(self, screen):
 		pygame.draw.rect(screen, (255, 107, 53), (self.position.x, self.position.y , 40, 40))
 
 
-	def get_input(self):
+	def input(self):
 		keys = pygame.key.get_pressed()
 
 		if keys[pygame.K_a]: self.move_left()
 		if keys[pygame.K_d]: self.move_right()
 		if keys[pygame.K_w]: self.move_up()
 		if keys[pygame.K_s]: self.move_down()
-		if keys[pygame.K_SPACE]: self.shoot()
+		if keys[pygame.K_SPACE]: self.check_shot_cooldown()
 
 	def clamp_player_pos(self):
 		if self.position.y <= 300: 
@@ -43,5 +46,13 @@ class PlayerController():
 	def move_down(self):
 		self.position.y += 5
 
-	def shoot(self):
+
+	def check_shot_cooldown(self):
+		current_time = pygame.time.get_ticks()
+		if current_time - self.last_shot_time >= self.cooldown:
+			self.shoot_bullet()
+			self.last_shot_time = current_time
+
+	def shoot_bullet(self):
 		ProjectileManager.register_player_bullet(self.position)
+		
